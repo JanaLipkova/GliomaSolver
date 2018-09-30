9833f29f26164c8017ddef5f8b0fec90d55f4a91
Jana Lipkova <jana.lipkova@tum.de>
2018-09-30 13:17:56 +0200

    add reamd.md for brain deformation example

--- makefile/BrainDeform/run_lrz.sh
+++ makefile/BrainDeform/run_lrz.sh
@@ -2,13 +2,14 @@
 #SBATCH -o myjob.%j.%N.out
 #SBATCH -D .
 #SBATCH -J DefTest
-#SBATCH --get-user-env 
+#SBATCH --get-user-env
 #SBATCH --clusters=mpp3
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-# cpus per task max value: mpp1 = 16, mpp2=28, mpp3=64 
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=32
+# cpus per task max value: mpp1 = 16, mpp2=28, mpp3=64
 #SBATCH --export=NONE
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
+
 
 # modules:
 source /etc/profile.d/modules.sh
@@ -25,30 +26,31 @@ export LD_LIBRARY_PATH=$LIB_BASE/tbb40_20120613oss/build/linux_intel64_gcc_cc4.6
 export LD_LIBRARY_PATH=$LIB_BASE/hypre-2.10.0b/src/hypre/lib/:$LD_LIBRARY_PATH
 
 # Threads:
-export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+N=2
+export OMP_NUM_THREADS=$N
 
 # brain simulation set up
 program=brain
 model=deform
-adaptive=0
 verbose=1
 profiler=1
-pID=100
 bDumpIC=0
-dumpfreq=1
+dumpfreq=2
 vtk=1
 CFL=0.8
-tend=600
+
+# model parameters
+tend=10
 rho=0.012
-Dw=1.3e-04
+Dw=1.3e-03
 kCSF=100
 kWM=10
 kGM=1
 bMobility=0
+ICtype=1
 
 echo "In the directory: $PWD"
-echo "Running program with total $SLURM_NTASKS MPI tasks, each with $SLURM_CPUS_PER_TASK threads."
+echo "Running program on $SLURM_NODES nodes with total $SLURM_NTASKS MPI tasks, each with $SLURM_CPUS_PER_TASK resp $N threads."
 
-
-mpirun -n 1  ./$program -nthreads $SLURM_CPUS_ON_NODE -model $model -verbose $verbose -profiler $profiler -pID $pID -vtk $vtk -bDumpIC $bDumpIC -dumpfreq $dumpfreq -CFL $CFL -tend $tend -rho $rho -Dw $Dw -kCSF $kCSF -kWM $kWM -kGM $kGM -bMobility $bMobility
+mpirun -np 64 ./$program -nthreads $N -model $model -verbose $verbose -profiler $profiler -PatFileName $PatFileName -vtk $vtk -bDumpIC $bDumpIC -dumpfreq $dumpfreq -CFL $CFL -tend $tend -rho $rho -Dw $Dw -kCSF $kCSF -kWM $kWM -kGM $kGM -bMobility $bMobility -ICtype $ICtype
 
