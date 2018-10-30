e5d70d733c1a15008c52f784348f2114d7895c1b
Jana <lipkovaj@ethz.ch>
2018-10-30 15:28:04 +0100

    convert nii2dat unix usage

--- ProcessingData/source/convert_folder_content_nii2dat.m
+++ ProcessingData/source/convert_folder_content_nii2dat.m
@@ -7,7 +7,7 @@
 %
 %===================================
 
-% function convert_folder_content_nii2dat
+function convert_folder_content_nii2dat(inputPath)
 
 addpath('../lib/toolbox_matlab_nifti')
 addpath('../lib/NIfTI_20140122/')
@@ -15,37 +15,60 @@ addpath('../lib/vi');
 addpath('../lib/Matlab2C/matrixMatlab2Cpp/matlab/')
 addpath('../lib/')
 
-inputPath =  '../../../DataForPaper/Atlas/';
-outputPath = inputPath;
+'running matlab function'
+
+% inputPath=[inputPath,'/']
+% inputPath = num2str(inputPath)
+
+
+% inputPath =  '../../Examples/PatientSpecificInference/InputData/';
+outputPath = [inputPath(1:end-1),'_dat/']
+
+bRotate = 1;
 
 if( exist(outputPath,'dir') == 0 )
     mkdir(outputPath)
 end;
 
-bRotate = 0;
+if( exist(outputPath,'dir') == 0 )
+    mkdir(outputPath)
+end;
+
 
 %4) i)  get names of files in the input folder
 %   ii) convert them to nii
 %   iii) save nii to output folder
 files = dir(inputPath);
-filesNames = {files.name};
+filesNames = {files.name}
 filesNames = filesNames(~ismember(filesNames,{'.','..','.DS_Store'}));
+
+fprintf('\n Converting files: \n------------------\n');
 
 for i = 1:length(filesNames)
     
-    inFilename  = filesNames{i}
-    outFilename = [inFilename(1:end-3),'dat'];
+    inFilename  = filesNames{i};
+    fprintf('%s \n', inFilename);
     
-    nii_data = MRIread([inputPath,inFilename]);
-    data = nii_data.vol;    
-    
-    if(bRotate)
-        data = rotate90_3D(data,1);
-        data = rotate90_3D(data,1);
-        data = rotate90_3D(data,1);
+    if(~isempty(strfind(inFilename,'nii')))
+            
+        if( inFilename(end-3:end) == '.nii')
+            outFilename = [inFilename(1:end-3),'dat'];
+        else if( inFilename(end-6:end) == '.nii.gz')
+                outFilename = [inFilename(1:end-6),'dat'];
+            end;
+        end;
+        
+        nii_data = MRIread([inputPath,inFilename]);
+        data = nii_data.vol;
+        
+        if(bRotate)
+            data = rotate90_3D(data,1);
+            data = rotate90_3D(data,1);
+            data = rotate90_3D(data,1);
+        end;
+        
+        typeId = 1; % 0 double, 1 float
+        writeMatrix(data,[outputPath,outFilename],typeId);
     end;
-   
-    typeId = 1; % 0 double, 1 float
-    writeMatrix(data,[outputPath,outFilename],typeId);
 end;
 
